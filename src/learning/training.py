57968427f997ada58fc8b6dbfc501c10ab704260
Zuzu3290 <zuhairmuhammad16@gmail.com>
2026-05-16 12:04:03 +0200

    tonic is kept, init provides the ideal interface for allowing interactions between packages

--- src/learning/training.py
+++ src/learning/training.py
@@ -7,6 +7,9 @@ import torch
 from snntorch import functional as SF
 from skeleton import Settings
 
+cfg = Settings()
+device = torch.device(cfg.DEVICE)
+
 class SNNTrainer:
  
     def __init__(self, model, train_loader, cfg: Settings, device: torch.device):
