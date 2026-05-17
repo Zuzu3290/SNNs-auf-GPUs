f28331460b584fbd89433f60181834d0fbeaa423
SaadSadekMansoor <saadmansoor495@gmail.com>
2026-05-17 21:56:41 +0400

    feat: migrate to SpikingJelly, implement .long() fix for CUDA, and update trainer metrics

--- src/learning/___init__.py
+++ src/learning/___init__.py
@@ -1,4 +1,4 @@
-from .frameworks.snn_torch import SNN
+from .frameworks.snn_spikingjelly import SNN
 from .inference import SNNTester
 from .training import SNNTrainer
 from .data_pipeline import main as load_data
