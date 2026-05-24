89f29525e14c16be259bf7a5f1bf7837444c8d1b
Zuzu3290 <zuhairmuhammad16@gmail.com>
2026-05-24 18:28:21 +0200

    Spikingjelley working

--- src/learning/frameworks/__init__.py
+++ src/learning/frameworks/__init__.py
@@ -1,3 +1,5 @@
-from .snn_spikingjelly import SNN
+from .snn_spikingjelly import SNN_SJ
+from .snn_norse import SNN_NORSE
+from .snn_torch import SNN_TORCH
 
-__all__ = ['SNN_TORCH']
+__all__ = ['SNN_TORCH', 'SNN_NORSE', 'SNN_SJ']
