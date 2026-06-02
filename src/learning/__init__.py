bf1d4ace51f61ffbbaa2c1225bf370008ba89e0a
Haseeb <haseebravian756@gmail.com>
2026-06-02 20:18:44 +0200

    update init py for learning

--- src/learning/__init__.py
+++ src/learning/__init__.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))  # project root → skeleton
+sys.path.insert(0, str(Path(__file__).parent.parent))          # src/ → learning, compiler
+
 from .frameworks.snn_torch import SNN_TORCH
 from .frameworks.snn_norse import SNN_NORSE
 from .inference import SNNTester
